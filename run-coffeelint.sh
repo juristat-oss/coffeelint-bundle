bee591dd8febe2e503f45bedb1fb8fa5acd8a955
Ben Chociej <ben@chociej.io>
2015-10-22 14:24:49 -0500

    initial

---
+++ run-coffeelint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+COFFEELINT="$(find node_modules -executable -name 'coffeelint' | head -n1)"
+CONFIG="$(find node_modules -type d -name 'coffeelint-bundle' | head -n1)/coffeelint.json"
+"${COFFEELINT}" -c -f "${CONFIG}" ${@}
