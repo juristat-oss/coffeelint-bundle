576da7423314887b272a4390f1b49d574673d690
Ben Chociej <ben@chociej.io>
2015-11-13 12:42:53 -0600

    just publish a coffeelint.json

--- run-coffeelint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-COFFEELINT="$(find "$(npm bin)" -executable -name 'coffeelint' | head -n1)"
-CONFIG="$(find node_modules -type d -name 'coffeelint-bundle' | head -n1)/coffeelint.json"
-
-"${COFFEELINT}" -c -f "${CONFIG}" ${@}
